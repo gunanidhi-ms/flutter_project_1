b38c66e0feb756a70fa7cf08a4428c48858d9795
J Suhas <suhasjayaram03@gmail.com>
2025-07-20 19:38:03 +0530

    did some chnahges to color of text and added navigation to sign up page

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const SignupPage(), // Start from login
+      home: const LoginScreen(), // Start from login
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project_1/db_helper.dart';
 import '../widgets/custom_scaffold.dart';
+import 'signup_page.dart';
 
 
 class LoginScreen extends StatefulWidget{
@@ -26,7 +27,7 @@ class _LoginScreenState extends State<LoginScreen>{
               style: TextStyle(
                 fontSize: 32,
                 fontWeight: FontWeight.bold,
-                color:Colors.white,
+                color:const Color.fromARGB(255, 4, 22, 58),
 
               ),
               ),
@@ -36,7 +37,7 @@ class _LoginScreenState extends State<LoginScreen>{
                 style: TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.bold,
-                  color:Colors.white70,
+                  color:Colors.black,
                 ),
               ),
               const SizedBox(height:30),
@@ -127,6 +128,30 @@ class _LoginScreenState extends State<LoginScreen>{
                   style: TextStyle(
                     fontSize: 16,color:Colors.white),
                   ),
+                  ),
+                  const SizedBox(height:10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account?",
+                        style:TextStyle(color:Colors.black),
+                      ),
+                      TextButton(
+                        onPressed: (){
+                          Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) =>SignupPage()),
+                          );
+                        },
+                        child: Text(
+                         "sign up",
+                         style: TextStyle(
+                          color:Colors.black,
+                          fontWeight: FontWeight.bold),
+                          ),
+                      ),
+                    ],
                   ),
           ],
 
